71d3682767978468478e3d9e96ed764a1880e3f5
alexzarrella <alexzarrella@yahoo.com>
2018-08-23 18:25:34 -0400

    working on search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import Form from "./components/form";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Search For A Recipe</h1>
+        </header>
+        <Form />
+      </div>
+    );
   }
 }
 
---
+++ src/components/form.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+const Form = () => (
+  <form>
+    <input type="text" />
+    <button>Search</button>
+  </form>
+);
+
+export default Form;
