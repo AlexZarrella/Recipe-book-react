a7df2f978dcdc374ff80ffa387aa755d96f16b05
alexzarrella <alexzarrella@yahoo.com>
2018-08-29 12:13:27 -0400

    class names

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -5,10 +5,22 @@ const Recipes = props => (
     <div className="row">
       {props.recipes.map(recipe => {
         return (
-          <div key={recipe.recipe_id}>
-            <img src={recipe.image_url} alt={recipe.title} />
-            <p>{recipe.title}</p>
+          <div className="col-md-4">
+            <div className="recipe__box">
+              <img
+                className="recipe__box-img"
+                src={recipe.image_url}
+                alt={recipe.title}
+              />
+              <div className="recipe__text">
+                <h5>{recipe.title}</h5>
+              </div>
+            </div>
           </div>
+          //   <div key={recipe.recipe_id}>
+          //     <img src={recipe.image_url} alt={recipe.title} />
+          //     <p>{recipe.title}</p>
+          //   </div>
         );
       })}
     </div>
